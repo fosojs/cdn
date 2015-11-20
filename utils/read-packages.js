8ac5297a90c3f2a76b0c4e7b7b3fd3756fb452b9
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-11-21 00:50:06 +0200

    version patterns implemented.

--- utils/read-packages.js
+++ utils/read-packages.js
@@ -2,12 +2,27 @@
 
 var fs = require('fs');
 var path = require('path');
+var getPackageVersions = require('./get-package-versions');
+var semver = require('semver');
+
+function getMatchingVersion(pkgName, versionPattern) {
+  var versions = getPackageVersions(pkgName);
+  for (var index in versions) {
+    if (semver.satisfies(versions[index], versionPattern)) {
+      return versions[index];
+    }
+  }
+}
 
 module.exports = function(destPath, packages) {
   var bundle = 'window.ungPackages = window.ungPackages || {};';
   packages.forEach(function(pkg) {
-    bundle += ';ungPackages["' + pkg.name + '"]="' + pkg.version + '";';
-    var pkgPath = path.join(destPath, pkg.name, pkg.version);
+    var matchingVersion = getMatchingVersion(pkg.name, pkg.version);
+    if (!matchingVersion) {
+      throw new Error('No matching version found');
+    }
+    bundle += ';ungPackages["' + pkg.name + '"]="' + matchingVersion + '";';
+    var pkgPath = path.join(destPath, pkg.name, matchingVersion);
     pkg.files.forEach(function(relativeFilePath) {
       var filePath = path.join(pkgPath, relativeFilePath);
       bundle += fs.readFileSync(filePath, {encoding: 'utf-8'});
