4f40c060bfb6191e8ce8b175a06ed29b539ca069
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-12-31 01:29:13 +0200

    test coverage added visualization added.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const istanbul = require('gulp-istanbul');
+const plumber = require('gulp-plumber');
+
+gulp.task('pre-test', function() {
+  return gulp.src('app/**/*.js')
+    .pipe(istanbul({
+      includeUntested: true
+    }))
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function(cb) {
+  let mochaErr;
+
+  gulp.src('test/**/*.js')
+    .pipe(plumber())
+    .pipe(mocha({reporter: 'spec'}))
+    .on('error', (err) => mochaErr = err)
+    .pipe(istanbul.writeReports())
+    .on('end', () => cb(mochaErr));
+});
